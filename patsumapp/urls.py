cd731d98c2fafeb009814980f066f7ae012abf76
xanderwise <alex7490.ander@gmail.com>
2025-05-27 14:12:28 +0300

    patsumapp

---
+++ patsumapp/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+
+from patsumapp import views
+
+urlpatterns = [
+        path('admin/', admin.site.urls),
+        path('starter/', views.starter, name='starter'),
+        path('home/', views.index, name='index'),
+        path('about/', views.about, name='about'),
+        path('contact/', views.contact, name='contact'),
+        path('service/', views.service, name='service'),
+]
---
+++ patsumapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def starter(request):
+    return render(request, 'starter-page.html')
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def service(request):
+    return render(request, 'service.html')
